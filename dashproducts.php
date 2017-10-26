8904a54fc611eab93474925e2d2f85c1c0d3817b
Michael Dekker <git@michaeldekker.nl>
2017-10-26 02:38:31 +0200

    Bump to 2.0.1

--- dashproducts.php
+++ dashproducts.php
@@ -39,7 +39,7 @@ class DashProducts extends Module
     {
         $this->name = 'dashproducts';
         $this->tab = 'dashboard';
-        $this->version = '1.0.0';
+        $this->version = '2.0.1';
         $this->author = 'thirty bees';
 
         $this->push_filename = _PS_CACHE_DIR_.'push/activity';
