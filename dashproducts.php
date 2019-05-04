0ecd9e5a2c6e3c7782fb7503473bbdbca69761be
Markus Hitter <mah@jump-ing.de>
2019-05-04 14:14:32 +0200

    Add constructor entry 'tb_min_version'.
    This is a requirement introduced recently by the validation script.

--- dashproducts.php
+++ dashproducts.php
@@ -50,6 +50,7 @@ class DashProducts extends Module
         $this->displayName = $this->l('Dashboard Products');
         $this->description = $this->l('Adds a block with a table of your latest orders and a ranking of your products');
         $this->tb_versions_compliancy = '> 1.0.0';
+        $this->tb_min_version = '1.0.0';
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
     }
 
